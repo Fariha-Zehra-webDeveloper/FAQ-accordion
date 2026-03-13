7c9cbc37e6a2defa997f0880376885fc4f3ee75f
Fariha-Zehra-webDeveloper <farihazehra640@gamil.com>
2026-03-13 16:46:15 +0500

    FAQ accordion done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let allQues = document.querySelectorAll(".faq__question");
-let allAns = document.querySelectorAll(".faq__item p");
-let icons = document.querySelectorAll(".faq__icon");
+const allQues = document.querySelectorAll(".faq__question");
+const allAns = document.querySelectorAll(".faq__item p");
+const icons = document.querySelectorAll(".faq__icon");
 
 
 
 allQues.forEach(function(question){
     question.addEventListener("click",function(){
-         let isOPen = this.nextElementSibling.classList.contains("show-answer");
+       const answer = this.nextElementSibling;
+        const icon = this.querySelector(".faq__icon");
+         const isOPen = this.nextElementSibling.classList.contains("show-answer");
           allAns.forEach(ans => ans.classList.remove("show-answer"));
           icons.forEach(icon => icon.src = "./assets/images/icon-plus.svg");
           if(!isOPen){
